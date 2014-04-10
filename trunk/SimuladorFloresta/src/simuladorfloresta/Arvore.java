06288f3df15c311ba9bf6d8a5f6cb4221559b6f0
gustavo.sabel.gs@gmail.com <gustavo.sabel.gs@gmail.com@9755de94-4031-6782-abb8-34ee0437ba2e>
2014-04-10 22:36:44 +0000

    só pra ver se....

--- trunk/SimuladorFloresta/src/simuladorfloresta/Arvore.java
+++ trunk/SimuladorFloresta/src/simuladorfloresta/Arvore.java
@@ -5,5 +5,37 @@ public class Arvore {
     private int agua;
     private int luz;
     private int saisMinerais;
+
+    public int getAgua() {
+        return agua;
+    }
+
+    public void setAgua(int agua) {
+        this.agua = agua;
+    }
+
+    public int getLuz() {
+        return luz;
+    }
+
+    public void setLuz(int luz) {
+        this.luz = luz;
+    }
+
+    public int getSaisMinerais() {
+        return saisMinerais;
+    }
+
+    public void setSaisMinerais(int saisMinerais) {
+        this.saisMinerais = saisMinerais;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
     private int energia;
 }
