80341be336ff093978034521081696035f44eafb
gustavo.sabel.gs@gmail.com <gustavo.sabel.gs@gmail.com@9755de94-4031-6782-abb8-34ee0437ba2e>
2014-04-10 22:33:22 +0000

    Criado

---
+++ trunk/SimuladorFloresta/src/simuladorfloresta/Arvore.java
@@ -0,0 +1,9 @@
+
+package simuladorfloresta;
+
+public class Arvore {
+    private int agua;
+    private int luz;
+    private int saisMinerais;
+    private int energia;
+}
---
+++ trunk/SimuladorFloresta/src/simuladorfloresta/Main.java
@@ -0,0 +1,8 @@
+package simuladorfloresta;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+    }
+}
