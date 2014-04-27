818c4ea2ff1832f9b8a23d151b1b28764c83a746
gustavo.sabel.gs@gmail.com <gustavo.sabel.gs@gmail.com@9755de94-4031-6782-abb8-34ee0437ba2e>
2014-04-27 20:26:34 +0000

    Ambiente e Fotossintese rodando sincronizado

--- trunk/SimuladorFloresta/src/simuladorfloresta/Arvore.java
+++ trunk/SimuladorFloresta/src/simuladorfloresta/Arvore.java
@@ -7,7 +7,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class Arvore {
-    
+
     private final int TEMPO_ESPERA = 0;
     private int agua;
     private int luz;
@@ -27,13 +27,13 @@ public abstract class Arvore {
     private int saisFotossintese;
     private ArrayList<Galho> galhos;
     private EnumEtapaProcesso etapa;
-    
-    public Arvore(int tamanhoMax, 
-                  int raioMax, 
-                  int luzFot, 
-                  int aguaFot, 
-                  int saisFot,
-                  EnumEtapaProcesso etapa) {
+
+    public Arvore(int tamanhoMax,
+            int raioMax,
+            int luzFot,
+            int aguaFot,
+            int saisFot,
+            EnumEtapaProcesso etapa) {
         lock = new ReentrantLock();
         temAgua = lock.newCondition();
         temLuz = lock.newCondition();
@@ -46,12 +46,12 @@ public abstract class Arvore {
         this.galhos = new ArrayList();
         this.etapa = etapa;
     }
-    
+
     public Posicao getPosicao() {
         return posicao;
     }
-    
-    public EnumEtapaProcesso getEtapa(){
+
+    public EnumEtapaProcesso getEtapa() {
         return etapa;
     }
 
@@ -77,6 +77,100 @@ public abstract class Arvore {
 
     public int getSaisFotossintese() {
         return saisFotossintese;
+    }
+
+    /*public synchronized boolean retiraNutrientes(int qtdAgua, int qtdLuz, int qtdSais) throws InterruptedException {
+        lock.lock();
+        try {
+            while (agua < qtdAgua) {
+                if (!temAgua.await(TEMPO_ESPERA, TimeUnit.MILLISECONDS)) {
+                    return false;
+                }
+            }
+            while (luz < qtdLuz) {
+                if (!temLuz.await(TEMPO_ESPERA, TimeUnit.MILLISECONDS)) {
+                    return false;
+                }
+            }
+            while (saisMinerais < qtdSais) {
+                if (!temSaisMinerais.await(TEMPO_ESPERA, TimeUnit.MILLISECONDS)) {
+                    return false;
+                }
+            }
+            
+            return true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public synchronized void setNutrientes() {
+    }*/
+
+    public synchronized void retiraEnergia(int qtd) throws Exception {
+        /*while (energia < qtd) {
+         wait();
+         }*/
+
+        energia -= qtd;
+        msg("Retirou " + qtd + " energia. Total:" + energia);
+    }
+
+    public synchronized void setEnergia(int qtd) {
+        this.energia += qtd;
+        //notifyAll();
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    private void msg(String msg) {
+        //System.out.println(msg);
+    }
+
+    public String ImprimeDados() {
+        String saida = "";
+        saida += "Agua:" + this.agua + "\n";
+        saida += "Luz:" + this.luz + "\n";
+        saida += "Sais:" + this.saisMinerais + "\n";
+        saida += "Energia:" + this.energia + "\n";
+        return saida;
+    }
+
+    /*public void crescer() throws Exception{
+     if(this.energia >= 20){
+     for (Galho galho : galhos) {
+     if(galho.addFolha()){
+     this.retiraEnergia(20);
+     break;
+     }
+     }
+     }
+        
+     if(this.energia >= 50){
+     galhos.add(new Galho(20));
+     this.retiraEnergia(50);
+     }
+        
+     if(this.energia >= 10){
+     this.tamanho++;
+     this.retiraEnergia(10);
+     }
+     }*/
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public boolean setTamanho(int tamanho) {
+        if (this.tamanho >= this.tamanhoMax) {
+            return false;
+        }
+        this.tamanho += tamanho;
+        if (this.tamanho > this.tamanhoMax) {
+            this.tamanho = this.tamanhoMax;
+        }
+        return true;
     }
 
     public boolean retiraAgua(int qtd) throws Exception {
@@ -167,72 +261,5 @@ public abstract class Arvore {
         } finally {
             lock.unlock();
         }
-    }
-
-    public synchronized void retiraEnergia(int qtd) throws Exception {
-        /*while (energia < qtd) {
-            wait();
-        }*/
-        
-        energia -= qtd;
-        msg("Retirou " + qtd + " energia. Total:" + energia);
-    }
-
-    public synchronized void setEnergia(int qtd) {
-        this.energia += qtd;
-        //notifyAll();
-    }
-
-    public int getEnergia() {
-        return energia;
-    }
-    
-    private void msg(String msg) {
-        //System.out.println(msg);
-    }
-
-    public String ImprimeDados() {
-        String saida = "";
-        saida += "Agua:" + this.agua + "\n";
-        saida += "Luz:" + this.luz + "\n";
-        saida += "Sais:" + this.saisMinerais + "\n";
-        saida += "Energia:" + this.energia + "\n";
-        return saida;
-    }
-    
-    
-    
-    /*public void crescer() throws Exception{
-        if(this.energia >= 20){
-            for (Galho galho : galhos) {
-                if(galho.addFolha()){
-                    this.retiraEnergia(20);
-                    break;
-                }
-            }
-        }
-        
-        if(this.energia >= 50){
-            galhos.add(new Galho(20));
-            this.retiraEnergia(50);
-        }
-        
-        if(this.energia >= 10){
-            this.tamanho++;
-            this.retiraEnergia(10);
-        }
-    }*/
-
-    public int getTamanho() {
-        return tamanho;
-    }
-    
-    public boolean setTamanho(int tamanho){
-        if(this.tamanho >= this.tamanhoMax)
-            return false;
-        this.tamanho+= tamanho;
-        if(this.tamanho > this.tamanhoMax)
-            this.tamanho = this.tamanhoMax;
-        return true;
     }
 }
